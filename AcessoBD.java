ed72edaeec8a0e24fe7bffd80d29ad1b8dfa37f3
Victor Faria <40238468+Biscoitera@users.noreply.github.com>
2019-06-11 14:47:59 -0300

    JDBC part 01

---
+++ AcessoBD.java
@@ -0,0 +1,59 @@
+
+package trabalhoparte2;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class AcessoBD {
+    
+    static String url="jdbc:oracle:thin:VMDADOS:1521/Ora11GDev:XE";
+    static String usuario = "MOBILE_COOPERALFA";
+    static String senha = "engeman";
+    static Connection conexao;
+    
+    public static void conectar() throws SQLException{
+        conexao = DriverManager.getConnection(url,usuario,senha);
+    }
+    
+    public static void consultarLutador() throws SQLException{
+        String consulta = "SELECT * FROM Lutador";
+        Statement statement = conexao.createStatement();
+        ResultSet rs = statement.executeQuery(consulta);
+        
+        while(rs.next()){
+            JOptionPane.showMessageDialog(null,"Nome: " + rs.getString(1) + " nacionalidade: " + rs.getString(2));
+        }
+                
+    }            
+                
+                
+    public static void mostrarMetaInfoBD() throws SQLException{
+        DatabaseMetaData meta = conexao.getMetaData();
+        String fabricanteBD = meta.getDatabaseProductName();
+        String versaoBD = meta.getDatabaseProductVersion();
+        JOptionPane.showMessageDialog(null,fabricanteBD +"<==>"+versaoBD);
+    }
+    
+    public static void main(String[] args){
+        try {
+            conectar();
+            mostrarInfoBD();
+            //consultarLutador();
+            conexao.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(AcessoBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    private static void mostrarInfoBD() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
